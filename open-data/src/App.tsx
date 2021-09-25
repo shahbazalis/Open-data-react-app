d0dc57a31a23e8a335f7e303509fafda3816e7a6
Shahbaz Ali <engr.shahbaz92@gmail.com>
2021-09-25 11:31:07 +0300

    signin and signup pages created using material ui

--- open-data/src/App.tsx
+++ open-data/src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+  Redirect,
+} from "react-router-dom";
+import SignIn from "./Pages/signin";
+import SignUp from "./Pages/signup";
 
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
